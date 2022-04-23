c99df8f71f3a61b42ab396a014ea1059733d141f
加藤康夫 <yasuokatou@gmail.com>
2022-04-23 15:02:56 +0900

    flaskとの連動

--- pages/test/server/todo_server2_flask.py
+++ pages/test/server/todo_server2_flask.py
@@ -1,11 +1,8 @@
 # -*- coding:utf-8 -*-
 
-import importlib
-import inspect
 import logging
 import logging.handlers
 import pathlib
-import sys
 import pathlib
 
 from flask import Flask, request, jsonify
@@ -27,26 +24,6 @@ rh.setFormatter(Formatter('%(asctime)s %(thread)d-%(threadName)s %(name)s:%(line
 l.addHandler(rh)
 
 
-'''
-_myPath = pathlib.Path(__file__)
-sys.path.append(str(_myPath.parent / 'service'))
-_serviceList = [
-    importlib.import_module('todo_service').TodoService(),
-]
-_todoService = {}
-for svc in _serviceList:
-    for ite in inspect.getmembers(svc, inspect.ismethod):
-        m = ite[0]
-        if m.startswith('_'):
-            continue
-        if callable(getattr(svc, m)):
-            # TODO 重複登録時の判定が必要
-            _todoService[m] = svc
-            print('/{} at {}'.format(m, type(svc).__name__))
-
-def _getServiceMethod(path):
-    return getattr(_todoService[path], path)
-'''
 _todoService = TodoService2()
 _todoService.read_service_config()
 
